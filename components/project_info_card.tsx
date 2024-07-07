55d882dd9d29f9810dbf58d21d4127cd622d8add
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2024-07-07 17:21:56 +0800

    Make ProjectInfoCard Responsive

--- components/project_info_card.tsx
+++ components/project_info_card.tsx
@@ -13,11 +13,13 @@ interface ProjectCardProps {
 
 function ProjectInfoCard(props:ProjectCardProps) {
     return (
-        <div className={"flex flex-row space-x-2 h-full justify-center"}>
+        <div className={"flex flex-col lg:flex-row xl:flex-row space-x-2 h-full justify-center pt-3"}>
             <Image src={props.src} alt={props.alt} width={props.width} height={props.height} className={props.className}/>
-            <div className={"flex-1 justify-center"}>
+            <div className={"flex-1 justify-center text-center lg:text-left xl:text-left pt-2"}>
                 <p className={"font-bold text-lg"}>{props.name}</p>
-                {props.children}
+                <div className={"md-5"}>
+                    {props.children}
+                </div>
             </div>
         </div>
     );
