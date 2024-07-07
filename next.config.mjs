ecfce5e1b9c11899b9eec63138b58c88869b2f2c
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2024-07-07 16:53:46 +0800

    Modify build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'encrypted-tbn0.gstatic.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+    output: "export",
 };
 
 export default nextConfig;
