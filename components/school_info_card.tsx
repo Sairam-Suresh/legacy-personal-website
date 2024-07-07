b2575edaac2bcc8885510d15205638e7515b92ca
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2024-07-07 17:00:39 +0800

    Rounded corners for school logos

--- components/school_info_card.tsx
+++ components/school_info_card.tsx
@@ -13,7 +13,7 @@ interface SchoolCardProps {
 function SchoolInfoCard(props: SchoolCardProps) {
     return (
         <div className={"flex lg:flex-row xl:flex-row flex-col justify-items-center text-center space-x-2"}>
-            <Image src={props.src} alt={props.alt} width={props.width} height={props.height} className={"m-auto"}/>
+            <Image src={props.src} alt={props.alt} width={props.width} height={props.height} className={"m-auto rounded-lg"}/>
             <div className={"flex-1"}>
                 <p className={"font-bold text-lg"}>{props.name}</p>
                 <div className={"flex flex-col h-full w-full justify-content-center lg:justify-content-start xl:justify-content-start"}>
